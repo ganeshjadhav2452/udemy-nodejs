e23f6101dcd063f863052e1d2fcca598b836f416
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-08-12 14:11:22 +0530

    learnt how to send responses to the client

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-  console.log('hellow nodejs')
+ 
+if(req.url === '/home'){
+  res.setHeader('Content-Type','text/html')
+  res.write('<html>')
+  res.write('<header><title>Home page</title></header>')
+  res.write('<body><h1>Hello User Welcom to our website</h1></body>')
+  res.write('</html>')
+  
+  res.end()
+}else if(req.url === '/about'){
+  res.setHeader('Content-Type','text/html')
+  res.write('<html>')
+  res.write('<header><title>about page</title></header>')
+  res.write('<body><h1>Hello User Welcom to our about page</h1></body>')
+  res.write('</html>')
+  
+  res.end()
+}else if(req.url === '/node'){
+  res.setHeader('Content-Type','text/html')
+  res.write('<html>')
+  res.write('<header><title>node page</title></header>')
+  res.write('<body><h1>Hello User Welcom to our nodejs website</h1></body>')
+  res.write('</html>')
+  
+  res.end()
+}
 })
 
 
