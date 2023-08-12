07fd8162b8b4949044e51220bfc8e6c09f8c03bb
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-08-12 17:16:15 +0530

    rendering added data infile

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
 const http = require('http')
-
+const fs = require('fs')
 const server = http.createServer((req, res)=>{
+
+if(req.url === '/'){
+  fs.readFile('message.txt','utf-8',(error, data)=>{
+    console.log(data)
+    res.write('<html>')
+    res.write('<header><title>Home page</title></header>')
+    res.write(`<body><h1>${data}</h1><form method="POST" action="/message" ><input name="message" type="text" /> <button type="submit" >submit</button></form></body>`)
+    res.write('</html>')
+  })
+  
+  return res.end()
+}
+
  
-if(req.url === '/home'){
-  res.setHeader('Content-Type','text/html')
-  res.write('<html>')
-  res.write('<header><title>Home page</title></header>')
-  res.write('<body><h1>Hello User Welcom to our website</h1></body>')
-  res.write('</html>')
-  
-  res.end()
-}else if(req.url === '/about'){
-  res.setHeader('Content-Type','text/html')
-  res.write('<html>')
-  res.write('<header><title>about page</title></header>')
-  res.write('<body><h1>Hello User Welcom to our about page</h1></body>')
-  res.write('</html>')
-  
-  res.end()
-}else if(req.url === '/node'){
-  res.setHeader('Content-Type','text/html')
-  res.write('<html>')
-  res.write('<header><title>node page</title></header>')
-  res.write('<body><h1>Hello User Welcom to our nodejs website</h1></body>')
-  res.write('</html>')
-  
-  res.end()
+
+if(req.url === '/message'  && req.method === 'POST'){
+  const body = []
+  req.on('data',(chunkOfData)=>{
+    body.push(chunkOfData)
+
+  })
+  req.on('end',()=>{
+    body.join(',')
+    const parsedBody = Buffer.concat(body).toString()
+
+    const message = decodeURIComponent(parsedBody.replace(/\+/g, ' ')).replace('message=','');
+
+    
+    fs.writeFileSync('message.txt',message)
+    res.statusCode = 302;
+    res.setHeader('Location', '/')
+ 
+   return res.end()
+  })
+ 
 }
 })
 
